2301d5c087aadc6fb9022524e99bb1db212217cb
Marina Menezes <marinamenezesdeo@gmail.com>
2024-11-08 09:19:56 -0300

    Update db_config.js

--- backend/src/db_config.js
+++ backend/src/db_config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'db_mercado',
+    host: 'bzk3cn7ro6cv7ibhfqsz-mysql.services.clever-cloud.com',
+    user: 'us3lwoxhb12m1hiz',
+    password: 'J4WfFfl1oS7zJHApuBcC',
+    database: 'bzk3cn7ro6cv7ibhfqsz',
 });
 
 connection.connect((err) => {
